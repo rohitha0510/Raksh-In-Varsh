f5dbc225cea06e957961a53412f7df3cb0f0234f
Rohitha Chowdary <edararohitha13@gmail.com>
2025-05-18 20:47:29 +0530

    Create talwind.js

---
+++ talwind.js
@@ -0,0 +1,17 @@
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], 
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('./assets/HeroBg.jpg')",
+        
+       
+      }
+    },
+  },
+  plugins: [],
+}
